259a0ed664bb2ebf8afe8f6e499d0df247e2736a
dabpessoa <dabpessoa@gmail.com>
2017-07-31 21:23:44 -0300

    initial commit

---
+++ src/test/java/me/dabpessoa/test/RunAllTests.java
@@ -0,0 +1,12 @@
+package me.dabpessoa.test;
+
+/**
+ * Created by diego.pessoa on 28/07/2017.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({EasyHistoryTest.class})
+public class RunAllTests {
+
+
+
+}
